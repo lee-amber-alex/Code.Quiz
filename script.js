16699f7aa1a437a87f826c188be174dcb00156a9
Amber Lee <lee.amber.alex@gmail.com>
2020-09-27 09:48:39 -0700

    started from scratch, removed code, renamed variables.

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let startEl = document.getElementById("#start");
+let timerEl = document.getElementById("#timer");
+let optionsEl = document.querySelectorAll("option");
+let contEl = document.querySelectorAll(".container2");
+let questionsAll = document.getElementById("questions");
+let optionElA = document.getElementById("#optionA")
+let optionElB = document.getElementById("#optionB")
+let optionElC = document.getElementById("#optionC")
+let optionElD = document.getElementById("#optionD")
+let quesEl = [
+    q1 ="1) On the periodic table, what element is represented by the letters Na?",
+    q2 = "2) What constellation contains the Big Dipper?",
+    q3 = "3) What country is the third largest trade partner of the U.S.?",
+    q4 = "4) Who invented bifocals?",
+    quesAns = "optionA", "optionB", "optionC", "optionD", 
+];
+let quesAns2 = ["optionA", "optionB", "optionC", "optionD"];
