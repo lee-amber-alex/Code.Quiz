520cfe212180c454218e67667710b452aa2bd8ab
Amber Lee <lee.amber.alex@gmail.com>
2020-09-29 22:25:23 -0700

    Added correct questions.

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ startEl.addEventListener("click", function () {
   // *****QUESTION 1*****
   optionsEl.forEach(function (element) {
     label.textContent =
-      "1) On the periodic table, what element is represented by the letters Na?";
-    optionElA.textContent = "a. Magnesium";
-    optionElB.textContent = "b. Nitrogen";
-    optionElC.textContent = "c. Sodium ";
-    optionElD.textContent = "d. Neon";
+      "1)	What is the definition of “HTML”?";
+    optionElA.textContent = "a. Used exactly like CSS,";
+    optionElB.textContent = "b. Hypertext Markup Language.";
+    optionElC.textContent = "c. Used to style a web page. ";
+    optionElD.textContent = "d. Hyper Tracing Max Language";
     element.addEventListener("click", function (event) {
       if (event.target.matches("#optionC")) {
         optionsAll.style.display = "none";
@@ -59,11 +59,11 @@ startEl.addEventListener("click", function () {
       // 1-2-3a) If timer runs out, move to next question
       // *****QUESTION 2*****
       buttonNext.addEventListener("click", function () {
-        label.textContent = "2) What constellation contains the Big Dipper";
-        optionElA.textContent = "a. Ursa Major";
-        optionElB.textContent = "b. Orion";
-        optionElC.textContent = "c. Ursa Minor";
-        optionElD.textContent = "d. Lyra";
+        label.textContent = "2) What are semantic tags used for?";
+        optionElA.textContent = "a. Used to bold content.";
+        optionElB.textContent = "b. Used to create a new element.";
+        optionElC.textContent = "c. Used to create more accessibility.";
+        optionElD.textContent = "d. Does not exist.";
         optionsAll.style.display = "block";
       });
     });
@@ -88,12 +88,11 @@ startEl.addEventListener("click", function () {
     // *****QUESTION 3*****
     buttonNext.addEventListener("click", function () {
       label.textContent =
-        "3) What country is the third largest trade partner of the U.S.?";
-      optionElA.textContent = "a. Canada";
-      optionElB.textContent = "b. China";
-      optionElC.textContent = "c. Mexico";
-      optionElD.textContent = "d. Brazil";
-      optionsAll.style.display = "block";
+        "3) Which tag is used to create a paragraph in HTML?"
+      optionElB.textContent = "b. <h1>";
+      optionElC.textContent = "c. <div>";
+      optionElD.textContent = "d. <p>";
+      optionsAll.style.display = "<br>";
     });
     // Repeat step 2 & 2a.
     optionsEl.forEach(function (element) {
@@ -114,11 +113,11 @@ startEl.addEventListener("click", function () {
     // 1-2-3a) If timer runs out, move to next question
     // *****QUESTION 4*****
     buttonNext.addEventListener("click", function () {
-      label.textContent = "4) Who invented bifocals?";
-      optionElA.textContent = "a. Benjamin Franklin";
-      optionElB.textContent = "b. Thomas Edison";
-      optionElC.textContent = "c. Leo Baekeland";
-      optionElD.textContent = "d. Clarence Birdseye";
+      label.textContent = "4) Which is the best tag for a Header?";
+      optionElA.textContent = "a. <h1>";
+      optionElB.textContent = "b. <h6>";
+      optionElC.textContent = "c. <h3>";
+      optionElD.textContent = "d. <h4>";
       optionsAll.style.display = "block";
     });
     // Repeat step 2 & 2a.
